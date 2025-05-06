73dc98ef692927d46b596540170e8ff23a34a4f9
captBlackb3ard <captBlackb3ard@users.noreply.github.com>
2025-05-06 18:59:38 +0300

    Update

--- 4_data_lists.py
+++ 4_data_lists.py
@@ -16,8 +16,8 @@ List items are ordered, changeable, and allow duplicate values
 The order of list items cannot be changed - new items are added at the end of the list
 List items are indexed, starting at [0]
 """
-
-print("\nLists\n---------------------------------------------------------")
+#/**List Definition**/
+print("\nList Definition\n---------------------------------------------------------")
 frstlist = ["apple", "banana", "cherry"]
 print(f"The list frstlist is of type: {type(frstlist)}")
 print(f"List variable 'frstlist' defined with values {frstlist}")
@@ -35,6 +35,49 @@ print("\nUse the list() method to create lists")
 frthlist = list(("ape", "bear", "cheetah")) # note the double round-brackets
 print(frthlist)
 
-print(f"\nList items can accessed directly by using their index number, e.g., frstlist[1] will return {frstlist[1]}")
+#/**Accessing List Items**/
+print("\nAccessing List Items\n---------------------------------------------------------")
+print(f"List items can accessed directly by using their index number, e.g., frstlist[1] will return {frstlist[1]}")
 print(f"Negative indexing also works, e.g., frstlist[-1] will return {frstlist[-1]}")
 print(f"It is possible to use a range of indexes, e.g., scndlist[1:4] will return {scndlist[1:4]}")
+print(f"Use the 'in' keyword to determine if an item exists in a list, e.g., 'if apple in frstlist'")
+if "apple" in frstlist:
+    print("'apple' is in the fruit list")
+
+#/**List Item Manipulation**/
+print("\nList Item Manipulation\n---------------------------------------------------------")
+scndlist[3] = "dewberry"
+print(f"To change the value of a specific list item refer to the index number, e.g., scndlist[3] = 'dewberry' will return {scndlist}")
+thrdlist[1:3] = ["baboon", "chimpanzee"]
+print(f"It is possible to change the value of a range of items using 'thrdlist[1:3]' syntax {thrdlist}")
+frthlist.insert(2, "cow")
+print(f"Use the 'insert()' method to add an item at a specific index, e.g., frthlist.insert(2) will return {frthlist}")
+frthlist.append("deer")
+print(f"Use the 'append()' method to add an item at the end of the list, e.g., frthlist.append('value') will return {frthlist}")
+frthlist.remove("cow")
+print(f"Use the 'remove()' to delete a specific list item by value, e.g., frthlist.remove('cow') results in {frthlist}") #Only removes the first occurrence
+frthlist.pop(1)
+print(f"The 'pop()' method removes a specified index, e.g., frthlist.pop(1) results in {frthlist}") #The last item is removed if no index is specified
+del scndlist[4]
+print(f"Use the 'del' keyword to delete a specified index value, e.g, scndlist.del(4) results {scndlist}")
+print(f"Running 'del scndlist' will delete the list completely.")
+frstlist.clear()
+print(f"Use the 'clear()' method to empty a list, e.g., frstlist.clear() results in an empty list {frstlist}")
+fruitlist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+print(f"Use the 'sort()' method to alphanumerically ascending sort this list {fruitlist}")
+fruitlist.sort()
+print(f"Execute fruitlist.sort() will return {fruitlist}")
+print("Use fruitlist.sort(reverse = True) to sort in descending order")
+#Use the copy() method to copy lists
+foodlist = fruitlist.copy()
+print(f"foodlist is a copy of the fruitlist - {foodlist}")
+print("\nUse the 'extend()' method to join two lists")
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+print(f"List 1 - {list1} and List 2 - {list2}")
+list1.extend(list2)
+print(f"Joined lists {list1}")
+#Creating a list of fruits that contain 'a' in the name using List Compression
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+print(f"List compression created list {newlist}")
--- 4_data_tuples.py
+++ 4_data_tuples.py
@@ -3,3 +3,37 @@
 """
 Python has 4 built-in data types - Lists, Tuples, Sets, and Dictionary
 """
+
+"""
+Python has 4 built-in data types - Lists, Tuples, Sets, and Dictionary
+"""
+
+"""
+=====================================================================
+Tuples
+=====================================================================
+Used to store multiple items in a single variable
+A tuple is ordered and unchangable (immutable)
+Tuples are created using parentheses ()
+Tuples allow duplicate values
+Tuple items are indexed, starting at [0]
+"""
+
+#/**Tuple Definition**/
+print("\nTuple Definition\n---------------------------------------------------------")
+tuple1 = ("apple", "banana", "cherry")
+print(f"The tuple tuple1 is of type: {type(tuple1)}")
+print(f"Tuple variable 'tuple1' defined with values {tuple1}")
+#tuple2 has duplicates
+tuple2 = ("apple", "banana", "cherry", "apple", "cherry")
+print(f"Tuples can contain duplicate values - {tuple2}")
+print(f"\nUse the 'len()' method to determine the number of tuple items: {len(tuple2)}")
+#Tuples with one item will need a comma added, else Python will not recognize it as a tuple
+tuple0 = ("apple", )
+print(f"tuple0 has only one item {tuple0}")
+#Tuples can contain multiple data types
+tuple3 = ("abc", 34, True, 40, "male")
+print(f"\nTuples can contain a different data types: {tuple3}")
+#Use the tupple() constructor to create a tuple
+tuple4 = tuple(("alligator", "bear", "cheetah", "donkey"))
+print(f"Use the 'tuple()' constuctor to create a tuple - {tuple4}")
